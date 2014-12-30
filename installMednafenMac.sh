1c244326e9c1a4042155d44fec60a493b4292cfb
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-30 14:31:56 -0800

    make it grab latest mednafen automagically

--- installMednafenMac.sh
+++ installMednafenMac.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+#  $ curl https://raw2.github.com/ryanpcmcquen/mednafenMacInstall/master/installMednafenMac.sh | sh
+
 # Ryan P.C. McQuen | Everett, WA | ryan.q@linux.com
 
 # This program is free software: you can redistribute it and/or modify
@@ -20,36 +22,34 @@
 # with this program (most likely, a file named COPYING).  If not, see
 # <http://www.gnu.org/licenses/>.
 
+wget mednafen.sourceforge.net/releases/ -O ~/mednafen-release-page.html
+cat ~/mednafen-release-page.html | grep mednafen | grep .tar.bz2 | cut -d'"' -f2 | sed 's/\?download//' | \
+  cut -d/ -f5 > ~/mednafenReleaseVersion
 
-VERSION=${VERSION:-0.9.36.4}
+rm -v ~/mednafen-release-page.html
+export MEDNAFENTARBALL=${MEDNAFENTARBALL="$( tr -d '\n\r' < ~/mednafenReleaseVersion )"}
 
-## script to automatically install mednafen on mac os
-#
-#  this should work, but i haven't tested it:
-#
-#  $ curl https://raw2.github.com/ryanpcmcquen/mednafenMacInstall/master/installMednafenMac.sh | sh
-
-
-export CC=/usr/local/Cellar/gcc48/4.8.3/bin/gcc-4.8
-export CPP=/usr/local/Cellar/gcc48/4.8.3/bin/cpp-4.8
-export CXX=/usr/local/Cellar/gcc48/4.8.3/bin/c++-4.8
+export CC=/usr/local/Cellar/gcc48/4.8.4/bin/gcc-4.8
+export CPP=/usr/local/Cellar/gcc48/4.8.4/bin/cpp-4.8
+export CXX=/usr/local/Cellar/gcc48/4.8.4/bin/c++-4.8
 
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew update && brew upgrade
 brew tap homebrew/versions
 brew install gcc48 wget libcdio libsndfile sdl
 
-wget -N https://prdownloads.sourceforge.net/mednafen/mednafen-$VERSION.tar.bz2 -P ~/Downloads/
+wget -N https://prdownloads.sourceforge.net/mednafen/$MEDNAFENTARBALL -P ~/Downloads/
 
-tar xf ~/Downloads/mednafen-$VERSION.tar.bz2 -C ~/Downloads/
+tar xvf ~/Downloads/$MEDNAFENTARBALL -C ~/Downloads/
 cd ~/Downloads/mednafen/
 
 ./configure
 make
 make install clean
 
-rm ~/Downloads/mednafen-$VERSION.tar.bz2
-rm -rf ~/Downloads/mednafen/
+rm -v ~/Downloads/$MEDNAFENTARBALL
+rm -rfv ~/Downloads/mednafen/
+rm -v ~/mednafenReleaseVersion
 
 echo
 echo "To run a rom type 'mednafen NAMEOFROM' from the command line."
